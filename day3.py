53c7edcf2b87c519cbd46587fa6f4b0560b8e36d
Jiayu Yi <yijiayu@gmail.com>
2019-12-03 22:12:35 +0800

    Create Vec2D class

--- day3.py
+++ day3.py
@@ -1,25 +1,37 @@
-def follow(wire, origin=(0, 0)):
-    x, y = origin
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Vec2D:
+    i: int
+    j: int
+
+    def __add__(self, other):
+        return Vec2D(self.i + other.i, self.j + other.j)
+
+
+def follow(wire, origin=Vec2D(0, 0)):
+    p = origin
     for segment in wire:
         direction = segment[0]
         magnitude = int(segment[1:])
+        if direction == 'U':
+            delta = Vec2D(0, 1)
+        elif direction == 'D':
+            delta = Vec2D(0, -1)
+        elif direction == 'L':
+            delta = Vec2D(-1, 0)
+        elif direction == 'R':
+            delta = Vec2D(1, 0)
+        else:
+            raise ValueError(f'invalid direction: "{direction}"')
         for _ in range(magnitude):
-            if direction == 'U':
-                y += 1
-            elif direction == 'D':
-                y -= 1
-            elif direction == 'L':
-                x -= 1
-            elif direction == 'R':
-                x += 1
-            else:
-                raise ValueError(f'invalid direction: "{direction}"')
-            yield x, y
+            p = p + delta
+            yield p
 
 
-def manhattan_distance(coords):
-    x, y = coords
-    return abs(x) + abs(y)
+def manhattan_distance_from_origin(p: Vec2D):
+    return abs(p.i) + abs(p.j)
 
 
 if __name__ == '__main__':
@@ -29,4 +41,4 @@ if __name__ == '__main__':
     wire1_route = set(follow(wire1))
     wire2_route = set(follow(wire2))
     crossings = wire1_route & wire2_route
-    print(min(map(manhattan_distance, crossings)))
+    print(min(map(manhattan_distance_from_origin, crossings)))
